dcd34eb0f721bf0b57b9bdac84c05f2e5f5f2ec4
Adhidarma Hadiwinoto <me@adhisimon.org>
2025-08-14 17:43:34 +0700

    Prometheus initialization move to out of class

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,50 @@
 import mysql from 'mysql2';
-import { Counter, Gauge, register } from 'prom-client';
+import { Counter, Gauge } from 'prom-client';
 let instanceCreated = 0;
+
+// const metrics = async () => await register.metrics();
+
+const waitingForConnectionCounter = new Counter({
+  name: 'mysql_pool_waiting_for_connection_occured',
+  help: 'Number of mysql pool waiting for connection occured',
+  labelNames: ['pool_id']
+});
+
+const acquireConnectionCounter = new Counter({
+  name: 'mysql_pool_acquire_connection',
+  help: 'Number of mysql pool acquire connection occured',
+  labelNames: ['pool_id']
+});
+
+const connectionLimitGauge = new Gauge({
+  name: 'mysql_pool_connection_limit',
+  help: 'Number of connection limit on mysql pool connection',
+  labelNames: ['pool_id']
+});
+
+const activeConnectionGauge = new Gauge({
+  name: 'mysql_pool_active_connection',
+  help: 'Number of active connections on mysql pool',
+  labelNames: ['pool_id']
+});
+
+const connectionQueueLengthGauge = new Gauge({
+  name: 'mysql_pool_connection_queue_length',
+  help: 'Number of connection waiting in mysql pool queue',
+  labelNames: ['pool_id']
+});
+
+const highestConnectionQueueLengthGauge = new Gauge({
+  name: 'mysql_pool_highest_connection_queue_length',
+  help: 'Highest connection queue length on mysql pool',
+  labelNames: ['pool_id']
+});
+
+const freeConnectionGauge = new Gauge({
+  name: 'mysql_pool_free_connection',
+  help: 'Number of free connections on mysql pool',
+  labelNames: ['pool_id']
+});
 
 /**
  * @typedef PoolIdObject
@@ -44,17 +88,18 @@ class MySQLClientWMetrics {
     this.pool = this.poolCallbackStyle.promise();
 
     const connectionLimit = this.poolCallbackStyle.config.connectionLimit;
-    let queueLength = 0;
-    let highestConnectionQueueLength = 0;
-    let activeConnectionsCount = 0;
-    let freeConnectionsCount = 0;
+    connectionLimitGauge.set({ pool_id: this.poolId }, connectionLimit);
+
+    this.highestConnectionQueueLength = 0;
 
     const updateMetrics = () => {
-      activeConnectionsCount = this.poolCallbackStyle._allConnections?.length || 0;
-      freeConnectionsCount = this.poolCallbackStyle._freeConnections?.length || 0;
+      activeConnectionGauge.set({ pool_id: poolId }, this.poolCallbackStyle._allConnections?.length || 0);
+      freeConnectionGauge.set({ pool_id: poolId }, this.poolCallbackStyle._freeConnections?.length || 0);
 
-      queueLength = this.poolCallbackStyle._connectionQueue?.length || 0;
-      highestConnectionQueueLength = Math.max(highestConnectionQueueLength, queueLength);
+      const queueLength = this.poolCallbackStyle._connectionQueue?.length || 0;
+      connectionQueueLengthGauge.set({ pool_id: poolId }, queueLength);
+      this.highestConnectionQueueLength = Math.max(this.highestConnectionQueueLength, queueLength);
+      highestConnectionQueueLengthGauge.set({ pool_id: poolId }, this.highestConnectionQueueLength);
     };
 
     /**
@@ -70,85 +115,6 @@ class MySQLClientWMetrics {
         return false;
       }
     };
-
-    this.metrics = async () => await register.metrics();
-
-    const waitingForConnectionCounter = new Counter({
-      name: 'mysql_pool_waiting_for_connection_occured',
-      help: 'Number of mysql pool waiting for connection occured',
-      labelNames: ['pool_id']
-    });
-
-    const acquireConnectionCounter = new Counter({
-      name: 'mysql_pool_acquire_connection',
-      help: 'Number of mysql pool acquire connection occured',
-      labelNames: ['pool_id']
-    });
-
-    // eslint-disable-next-line no-new
-    new Gauge({
-      name: 'mysql_pool_connection_limit',
-      help: 'Number of connection limit on mysql pool connection',
-      labelNames: ['pool_id'],
-      collect () {
-        this.set(
-          { pool_id: poolId },
-          connectionLimit
-        );
-      }
-    });
-
-    // eslint-disable-next-line no-new
-    new Gauge({
-      name: 'mysql_pool_connection_queue_length',
-      help: 'Number of connection waiting in mysql pool queue',
-      labelNames: ['pool_id'],
-      collect () {
-        this.set(
-          { pool_id: poolId },
-          queueLength
-        );
-      }
-    });
-
-    // eslint-disable-next-line no-new
-    new Gauge({
-      name: 'mysql_pool_highest_connection_queue_length',
-      help: 'Highest connection queue length on mysql pool',
-      labelNames: ['pool_id'],
-      collect () {
-        this.set(
-          { pool_id: poolId },
-          highestConnectionQueueLength
-        );
-      }
-    });
-
-    // eslint-disable-next-line no-new
-    new Gauge({
-      name: 'mysql_pool_active_connection',
-      help: 'Number of active connections on mysql pool',
-      labelNames: ['pool_id'],
-      collect () {
-        this.set(
-          { pool_id: poolId },
-          activeConnectionsCount
-        );
-      }
-    });
-
-    // eslint-disable-next-line no-new
-    new Gauge({
-      name: 'mysql_pool_free_connection',
-      help: 'Number of free connections on mysql pool',
-      labelNames: ['pool_id'],
-      collect () {
-        this.set(
-          { pool_id: poolId },
-          freeConnectionsCount
-        );
-      }
-    });
 
     this.poolCallbackStyle.on('enqueue', () => {
       waitingForConnectionCounter.labels({ pool_id: poolId }).inc();
